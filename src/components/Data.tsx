d377020508afb7532f18ffa4e35f6ad2894417d7
Udita <kaushik.voumik66@gmail.com>
2025-09-01 18:35:50 +0530

    could finally render the data in the frontend

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -4,42 +4,72 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 const Data = () => {
 
-//let localStorageData = JSON.parse(localStorage.getItem('FormData'));
-const[storedData,setStoredData] = useState([]);
+type FormData ={
+  firstName: string,
+  Age: string,
+  email: string,
+  course: string
+}
+const[storedData,setStoredData] = useState<FormData[]>([]);
 useEffect(() => {
   let localStorageData = localStorage.getItem('FormData');
-    if(localStorageData)
+    if(localStorageData){
     setStoredData(JSON.parse(localStorageData));
+  console.log("type of storedData",typeof storedData);
+console.log("storedData", storedData)
+console.log("localStorageData", localStorageData )
+ // console.log("typeof localStorageData", typeof localStorageData);
+//  console.log("entries of localStorageData",localStorageData);
+//  const myArray = localStorageData.split(",");
+//  console.log(myArray);
+//  console.log("type of myArray",typeof myArray);
+  }
 }, [])
-const variable = localStorage.getItem('FormData');
+//const variable = localStorage.getItem('FormData');
+
   return (
-    (variable===null) ? 
-    (   <> 
-        <div className='NoData'> 
-        <p> No Data Yet</p>
-        <Link className="addNew" to='/studentForm'>Add New Data</Link>
+   <>
+    {!storedData ? (
+        <div className="NoData">
+          <p>No Data Yet</p>
+          <Link className="addNew" to="/studentForm">
+            Add New Data
+          </Link>
         </div>
-        </>
-    ) : 
-    (
-    <>
-    <div className='Data'> 
-    <p> Submitted Entries </p>
-    <Link className="addNew" id='dataBtn' to='/studentForm'>Add New Data</Link>
+      ) : (
+        <>
+          <div className="Data">
+            <p>Submitted Entries</p>
+            <Link className="addNew" id="dataBtn" to="/studentForm">
+              Add New Data
+            </Link>
+          </div>
+           <div className='grid grid-cols-6 font-extrabold'>
+            <p> SL NO.</p>
+            <p> NAME</p>
+            <p> AGE</p>
+            <p> EMAIL</p>
+            <p> COURSE</p>
+            <p> ACTIONS</p>
+          </div>
+        <div className='h-full w-full bg-amber-500 px-2 py-2'>
+         {storedData.map((item, i) => (
+  <div key={i} className='grid grid-cols-7'>
+    <p>{i+1}</p>
+    <p>{item.firstName}</p>
+    <p>{item.Age}</p>
+    <p className='text-ellipsis'>{item.email}</p>
+    <p>{item.course}</p>
+    <div className='flex ml-14'> 
+    <button className='h-fit w-fit px-2 py-2'>del</button>
+    <button className='h-fit w-fit px-2 py-2'>edit</button>
     </div>
-    
-    <div>
-        
-    {Object.keys(storedData).map((entry,index)=>(
-      <div key={index}>
-       <h3>{index}</h3>
+  </div>
+))}
       </div>
-    ))}
-    
-    </div>
     </>
-    )
-  )
-}
+    )}
+  </>
+)}
 
 export default Data;
