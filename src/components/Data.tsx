ba22f97a7d43c6037c111988e55dbf04b51abe04
Udita <kaushik.voumik66@gmail.com>
2025-09-02 15:23:02 +0530

    could delete items from the frontend

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -10,6 +10,7 @@ type FormData ={
   email: string,
   course: string
 }
+//const[data,setData] = useState<FormData[]>([]);
 const[storedData,setStoredData] = useState<FormData[]>([]);
 useEffect(() => {
   let localStorageData = localStorage.getItem('FormData');
@@ -26,9 +27,15 @@ useEffect(() => {
   }
 }, [])
 //const variable = localStorage.getItem('FormData');
-function handleClick()
+
+
+
+const handleClick = (i:number):void =>
 {
-  
+  setStoredData(existingData =>{
+    return existingData.filter((item, currenti) => currenti !==i)
+  })
+ 
 }
   return (
    <>
@@ -43,14 +50,14 @@ function handleClick()
       ) : (
         <>
        
+          
+        <div className='h-screen w-screen mt-20 '> 
           <div className="Data">
             <p>Submitted Entries</p>
             <Link className="addNew" id="dataBtn" to="/studentForm">
               Add New Data
             </Link>
-          </div>
-        <div className='h-screen w-screen mt-20 '> 
-
+          </div> 
            <select className='border-2 border-solid rounded-md'>
             <option className='rounded-md'> C++</option>
             <option className='rounded-md'> Java</option>
@@ -74,7 +81,7 @@ function handleClick()
     <p className='max-w-2 truncate'>{item.email}</p>
     <p>{item.course}</p>
     <div className='flex flex-col gap-1'> 
-    <button className='h-fit w-fit px-2 py-2 rounded-lg text-white bg-red-500 hover:bg-red-800' onClick={handleClick}>Del</button>
+    <button className='h-fit w-fit px-2 py-2 rounded-lg text-white bg-red-500 hover:bg-red-800' onClick={()=>handleClick(i)}>Del</button>
     <button className='h-fit w-fit px-2 py-2 rounded-lg text-white bg-green-400 hover:bg-green-600'>Edit</button>
     </div>
   </div>
